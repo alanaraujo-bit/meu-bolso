62832947bf76b6257ebc4ae80f1d9913763afdcc
Alan Araújo <alanvitoraraujo2a@gmail.com>
2025-07-18 23:22:07 -0300

    Atualiza schema.prisma corrigindo relação Transacao <-> Tag para N:N

--- prisma/migrations/20250713211003_make_categoria_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Transacao" DROP CONSTRAINT "Transacao_categoriaId_fkey";
-
--- AlterTable
-ALTER TABLE "Transacao" ALTER COLUMN "categoriaId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Transacao" ADD CONSTRAINT "Transacao_categoriaId_fkey" FOREIGN KEY ("categoriaId") REFERENCES "Categoria"("id") ON DELETE SET NULL ON UPDATE CASCADE;
