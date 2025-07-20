366ac0d721c4c73342b234fb516d992a06a02c3b
Alan Araújo <alanvitoraraujo2a@gmail.com>
2025-07-20 03:37:47 -0300

    att

--- check_env.ts
+++
@@ -1,4 +0,0 @@
-// Script para verificar as variáveis de ambiente no ambiente de execução
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
-console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
